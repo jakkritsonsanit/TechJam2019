9374a2a90b46fdf893e4940053953877fde26f47
wsirigate <workke306@gmail.com>
2019-10-30 15:07:16 +0700

    edit model

---
+++ tpot_exported_pipeline.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline, make_union
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.tree import DecisionTreeClassifier
+from tpot.builtins import StackingEstimator
+
+# NOTE: Make sure that the class is labeled 'target' in the data file
+tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
+features = tpot_data.drop('target', axis=1).values
+training_features, testing_features, training_target, testing_target = \
+            train_test_split(features, tpot_data['target'].values, random_state=42)
+
+# Average CV score on the training set was:0.9888402931777115
+exported_pipeline = make_pipeline(
+    PolynomialFeatures(degree=2, include_bias=False, interaction_only=False),
+    StackingEstimator(estimator=LogisticRegression(C=0.5, dual=True, penalty="l2")),
+    StackingEstimator(estimator=DecisionTreeClassifier(criterion="entropy", max_depth=4, min_samples_leaf=12, min_samples_split=7)),
+    LogisticRegression(C=25.0, dual=False, penalty="l1")
+)
+
+exported_pipeline.fit(training_features, training_target)
+results = exported_pipeline.predict(testing_features)
